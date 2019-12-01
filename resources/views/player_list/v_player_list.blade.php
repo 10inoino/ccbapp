58d0d18f11a2534b17d33a3d7a482b850dd6e497
Hikaru Inomata <inomatahi@icloud.com>
2019-12-02 08:20:58 +0900

    baseurl

--- resources/views/player_list/v_player_list.blade.php
+++ resources/views/player_list/v_player_list.blade.php
@@ -20,6 +20,8 @@
         ['山本 純也','yamamoto','Junichiro Iboshi','4','SG','キャリアコンサルティングバスケットボール部のキャプテン。大学では武蔵野大学のバスケットボール部のキャプテンとしても活躍。持ち前のスピードとジャンプシュートでチームを勝利に導く。','png']
     ];
 
+    $base_url = "http://" . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
+
     $array_js = json_encode($player_names);
 ?>
 
@@ -28,18 +30,18 @@
     <head>
         <mata charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <link rel="stylesheet" type="text/css" href="http://localhost/ccbapp/public/css/bootstrap.css">
-        <link rel="stylesheet" type="text/css" href="http://localhost/ccbapp/public/css/custom_style.css">
+        <link rel="stylesheet" type="text/css" href="<?= $base_url ?>/css/bootstrap.css">
+        <link rel="stylesheet" type="text/css" href="<?= $base_url ?>/css/custom_style.css">
         <title>Player List</title>
     </head>
     <body>
-        <script src="http://localhost/ccbapp/public/js/jquery-3.4.1.min.js"></script>
-        <script src="http://localhost/ccbapp/public/js/bootstrap.bundle.min.js"></script>
-        <script src="http://localhost/ccbapp/public/js/player_list.js"></script>
+        <script src="<?= $base_url ?>/js/jquery-3.4.1.min.js"></script>
+        <script src="<?= $base_url ?>/js/bootstrap.bundle.min.js"></script>
+        <script src="<?= $base_url ?>/js/player_list.js"></script>
         <script>
             $(document).ready( function(){
                 var players = <?php echo $array_js ?>;
-                console.log(players);
+                var base_url_js = "<?php echo "http://" . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI']; ?>";
 
                 $('.player_detail_btn').on('click',function(){
                     var btn_val = $(this).val();
@@ -49,7 +51,7 @@
                     $("#name-eng").text(players[btn_val][2]);
                     $("#position").text(players[btn_val][4]);
                     $("#desc").text(players[btn_val][5]);
-                    $('#modal-player-img').attr('src', 'http://localhost/ccbapp/public/img/' + players[btn_val][1] + '/top.' + players[btn_val][6]);
+                    $('#modal-player-img').attr('src', base_url_js + '/img/' + players[btn_val][1] + '/top.' + players[btn_val][6]);
                     $("#player_detail").modal('show');
                 });
             });
@@ -57,7 +59,7 @@
 
         <header class="m-0 logo-area">
             <div class="container text-center py-4">
-                <h1 class="text-center z-1"><a href="#"><img class="w-25" src="http://localhost/ccbapp/public/img/ccblogo.png"></a></h1>
+                <h1 class="text-center z-1"><a href="#"><img class="w-25" src="<?= $base_url ?>/img/ccblogo.png"></a></h1>
             </div>
         </header>
         <nav class="navbar navbar-expand-md navbar-dark bg-dark sticky-top">
@@ -89,13 +91,13 @@
                         </ol>
                         <div class="carousel-inner">
                             <div class="carousel-item active">
-                                <img class="img-fluid" src="http://localhost/ccbapp/public/img/carousel/pic0.png">
+                                <img class="img-fluid" src="<?= $base_url ?>/img/carousel/pic0.png">
                             </div>
                             <div class="carousel-item">
-                                <img class="img-fluid" src="http://localhost/ccbapp/public/img/carousel/pic1.png">
+                                <img class="img-fluid" src="<?= $base_url ?>/img/carousel/pic1.png">
                             </div>
                             <div class="carousel-item">
-                                <img class="img-fluid" src="http://localhost/ccbapp/public/img/carousel/pic2.png">
+                                <img class="img-fluid" src="<?= $base_url ?>/img/carousel/pic2.png">
                             </div>
                         </div>
                         <a class="carousel-control-prev" href="#main_visual" role="button" data-slide="prev">
@@ -116,7 +118,7 @@
                             <?php foreach($player_names as $key => $player): ?>
                                 <div class="col-md-4">
                                     <div class="card mb-3">
-                                        <img src="http://localhost/ccbapp/public/img/<?= $player[1] ?>/top.<?= $player[6] ?>" alt="" id="player-img" class="img-fluid"  onerror="this.src='http://localhost/ccbapp/public/img/no_image.png'">
+                                        <img src="<?= $base_url ?>/img/<?= $player[1] ?>/top.<?= $player[6] ?>" alt="" id="player-img" class="img-fluid"  onerror="this.src='<?= $base_url ?>/img/no_image.png'">
                                         <div class="card-body d-flex justify-content-between">
                                             <h4 class="card-title"><?= $player[0] ?></h4>
                                             <button type="button" class="player_detail_btn btn btn-primary" value="<?= $key ?>">詳しく見る</button>
@@ -141,7 +143,7 @@
                         <div class="modal-body">
                             <div class="row">
                                 <div class="col-md-4">
-                                    <img src="" alt="" id="modal-player-img" class="img-thumbnail" onerror="this.src='http://localhost/ccbapp/public/img/no_image.png'">
+                                    <img src="" alt="" id="modal-player-img" class="img-thumbnail" onerror="this.src='<?= $base_url ?>/img/no_image.png'">
                                 </div>
                                 <div class="col-md-8 pt-4">
                                     <div class="w-100">
