b0c358f61eea1dfcf6724a2eb33ab38171af5519
Hikaru Inomata <inomatahi@icloud.com>
2019-12-03 23:47:50 +0900

    base_urlを本番用に変更

--- resources/views/player_list/v_player_list.blade.php
+++ resources/views/player_list/v_player_list.blade.php
@@ -20,8 +20,8 @@
         ['山本 純也','yamamoto','Junya Yamamoto','0','PG/SG','NEXUS柳島さん担当。高校バスケ名門校の土浦日大高校出身。時には落ち着いたPGとしてチームの司令塔となり、時には持ち前のドライブとガッツで果敢にゴールにアタックする。']
     ];
 
-    //$base_url = "https://" . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
-    $base_url = "http://localhost/ccbapp/public/";
+    $base_url = "https://" . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
+    //$base_url = "http://localhost/ccbapp/public/";
 
     $array_js = json_encode($player_names);
 ?>
